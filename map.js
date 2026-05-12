27894791e515933c435e17dd31b5bc064be7b4e2
romyramrakhyani <romyramrakhyani19@gmail.com>
2026-05-12 15:06:49 -0600

    changed token

--- map.js
+++ map.js
@@ -3,13 +3,16 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 // Set your Mapbox access token
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9teXJhbXJha2h5YW5pIiwiYSI6ImNtcDMzd2lpZTA4MGkycm9vNWRjYmhkeGIifQ.2wK8hH7tNx-YUdRqK3z0Kw';
 
+// Initialize the map
 const map = new mapboxgl.Map({
-  container: 'map', // This MUST match the id in your index.html
-  style: 'mapbox://styles/mapbox/streets-v12',
+  container: 'map', // ID of the div in your index.html
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
   center: [-71.09415, 42.36027], // [longitude, latitude]
-  zoom: 12,
-  minZoom: 5,
-  maxZoom: 18
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
 });
 
-console.log('Map initialized successfully!');
+map.on('load', () => {
+    console.log('Map has successfully loaded!');
+});
