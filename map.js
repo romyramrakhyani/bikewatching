5e01af65eb6689570cd01031d3a57885a9b80d51
romyramrakhyani <romyramrakhyani19@gmail.com>
2026-05-12 14:46:31 -0600

    map

--- map.js
+++ map.js
@@ -1,17 +1,16 @@
-// map.js
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
-// Set your Mapbox access token
-mapboxgl.accessToken = 'pk.YOUR_ACTUAL_TOKEN_HERE';
+// Check that Mapbox GL JS is loaded
+console.log('Mapbox GL JS Loaded:', mapboxgl);
 
-// Initialize the map
+// Replace with YOUR token
+mapboxgl.accessToken = 'pk.eyJ1Ijo...'; 
+
 const map = new mapboxgl.Map({
-  container: "map", // ID of the div in index.html
-  style: 'mapbox://styles/mapbox/streets-v12', // Basemap style
-  center: [-71.09415, 42.36027], // [longitude, latitude]
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
   zoom: 12,
-  minZoom: 5,
-  maxZoom: 18
 });
 
 console.log('Mapbox GL JS Loaded:', mapboxgl);
